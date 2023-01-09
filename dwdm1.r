d0b1f52ff4b402712ebe232a05ada6abc3191e75
Kokila25 <122247920+Kokila25@users.noreply.github.com>
2023-01-09 13:25:04 +0530

    Add files via upload

---
+++ dwdm1.r
@@ -0,0 +1,22 @@
+fruits<-list("apple","banana","orange")
+for(x in fruits) {
+  print(x)
+  }
+
+val=1
+while (val<=5) {
+  print(val)
+  val=val+1
+  
+}
+
+val=1
+repeat
+{
+  print(val)
+  val=val+1
+  if (val>5){
+    break
+  }
+} 
+
